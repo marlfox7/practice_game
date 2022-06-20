7ebe88c1428b0c0e0afd74612aba22ba71bef613
marlfox7 <46537627+marlfox7@users.noreply.github.com>
2022-06-20 11:12:02 -0700

    Create Main.hpp

---
+++ Main.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string input = "test"
+  
+  cout << "Welcome to a game" << endl;
+  cout << "Choose your class" << endl;
+  
+  return 0;
+}
