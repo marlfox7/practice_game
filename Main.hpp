61edf6a511a7edf2ee95b8e776bef58b5be5c25c
marlfox7 <46537627+marlfox7@users.noreply.github.com>
2023-01-21 21:01:54 -0800

    Update Main.hpp

--- Main.hpp
+++ Main.hpp
@@ -4,10 +4,21 @@
 using namespace std;
 
 int main() {
-  string input = "test"
+  string name = "test";
+  string userInput = "test";
   
-  cout << "Welcome to a game" << endl;
-  cout << "Choose your class" << endl;
+  cout << "Welcome to a game" << endl << "If you'd like to play, enter \" 1 \" . Otherwise the game will close." << endl;
+  cin >> userInput;
+  cout << userInput;
+  
+  if (userInput == "1") {
+    cout << "Enter your character's name: \n";
+    cin >> name;
+    cout << "You have chosen: " << name << "." endl;
+  }
+  else {
+    cout << "Closing the game: Thank you for playing!" << endl;
+  }
   
   return 0;
 }
